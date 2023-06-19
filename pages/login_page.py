47bf38baeaaefde71e1e5aa20d18e77662862ea3
Tregubov <kirill.tregubov1998@gmail.com>
2023-06-19 15:40:24 +0300

    Added Login and Register form checking

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
+            "This Login link is not valid"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),\
+            "There is no Login form on this link"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            "There is no Register form on this link"
