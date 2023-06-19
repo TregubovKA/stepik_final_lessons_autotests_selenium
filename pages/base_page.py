6d07d80a10bde4b3ce0da05f2518cea9b04288bc
Tregubov <kirill.tregubov1998@gmail.com>
2023-06-19 22:24:26 +0300

    Added methods from main_page to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -8,8 +9,20 @@ class BasePage():
         self.browser = browser
         self.url = url
         # self.browser.implicitly_wait(timeout)
-    def open(self):
-        self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException)\
+                .until_not(EC.presence_of_element_located((how,what)))
+        except TimeoutException:
+            return False
+
+        return True
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -26,11 +39,10 @@ class BasePage():
 
         return False
 
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException)\
-                .until_not(EC.presence_of_element_located((how,what)))
-        except TimeoutException:
-            return False
+    def open(self):
+        self.browser.get(self.url)
 
-        return True
+    def should_be_login_link(self):
+        assert \
+            self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            "Login link is not presented"
