1f5cdb756b55aa3578d30d62aca08148dcdf9001
Tregubov <kirill.tregubov1998@gmail.com>
2023-06-19 18:42:54 +0300

    added parametrized tests with xfail

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,8 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_LINK = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    NAME_OF_A_BOOK = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRICE_OF_A_BOOK = (By.CSS_SELECTOR, "div.alertinner p strong")
